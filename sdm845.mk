a8e269fa42a2efff95bc02d961d14f85cadfbcfc
Henrique Silva <jhenrique09.mcz@hotmail.com>
2020-11-19 21:50:18 +0530

    sdm845-common: Don't build LiveDisplay
    Change-Id: I2a93f55f0b7a4a4992a475287a40b1f8a15b0cc4

--- sdm845.mk
+++ sdm845.mk
@@ -294,10 +294,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0 \
     android.hardware.light@2.0.vendor
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
