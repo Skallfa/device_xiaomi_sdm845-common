ea311552af99cd807821d4b45d31606996d67654
kssrao13882 <kssrao13882@gmail.com>
2022-03-31 07:03:28 +0000

    sdm845-common: Import Pixel excluisve features
    Signed-off-by: krtonia <krtonia@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -227,9 +227,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
+
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
 
 # Health
 PRODUCT_PACKAGES += \
