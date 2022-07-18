7a7beedd75d722604ff1371cc223d86fc9b0b737
Ivan Vecera <ivan@cera.cz>
2022-07-18 19:32:13 +0200

    sdm845-common: Add shim for missing symbol in lib-imsvideocodec.so
    Signed-off-by: krtonia <krtonia@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,12 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := lib-imsvtshim.cpp
-LOCAL_MODULE := lib-imsvtshim
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES := lib-watermarkshim.cpp
 LOCAL_MODULE := lib-watermarkshim
 LOCAL_MODULE_TAGS := optional
