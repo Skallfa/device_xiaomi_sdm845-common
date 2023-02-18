af62d76e02cb6e5db1d336588e57ae94594dec22
Dmitrii <bankersenator@gmail.com>
2023-02-18 16:15:00 +0530

    sdm845-common: Switch to trb_clang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -38,8 +38,8 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 TARGET_KERNEL_CONFIG := vendor/xiaomi/mi845_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := proton
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
+TARGET_KERNEL_CLANG_VERSION := trb_clang
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/trb_clang
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm845
