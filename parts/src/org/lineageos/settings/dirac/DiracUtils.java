ee4581c660ba28cabc01dddbc210c5024f2ea372
PSKMaxi <123083334+PSKMaxi@users.noreply.github.com>
2023-02-19 21:57:29 +0530

    Update DiracUtils.java

--- parts/src/org/lineageos/settings/dirac/DiracUtils.java
+++ parts/src/org/lineageos/settings/dirac/DiracUtils.java
@@ -130,8 +130,19 @@ public class DiracUtils {
     public void setHeadsetType(int paramInt) {
         mDiracSound.setHeadsetType(paramInt);
     }
-
-    protected static void setScenario(int sceneInt) {
+    
+        protected static void setScenario(int sceneInt) {
         mDiracSound.setScenario(sceneInt);
+    }        
+
+    protected static boolean getHifiMode() {
+        AudioManager audioManager = mContext.getSystemService(AudioManager.class);
+        return audioManager.getParameters("hifi_mode").contains("true");
+    }
+
+    protected static void setHifiMode(int paramInt) {
+        AudioManager audioManager = mContext.getSystemService(AudioManager.class);
+        audioManager.setParameters("hifi_mode=" + (paramInt == 1 ? true : false));
+        mDiracSound.setHifiMode(paramInt);
     }
 }
