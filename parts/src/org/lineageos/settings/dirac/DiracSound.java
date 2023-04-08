b9b652de4978c333e5d4e4641e82e544376a1c2e
PSKMaxi <123083334+PSKMaxi@users.noreply.github.com>
2023-04-08 10:57:08 +0530

    Update DiracSound.java

--- parts/src/org/lineageos/settings/dirac/DiracSound.java
+++ parts/src/org/lineageos/settings/dirac/DiracSound.java
@@ -37,14 +37,16 @@ public class DiracSound extends AudioEffect {
     }
 
     public int getMusic() throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
+            IllegalArgumentException, UnsupportedOperationException,
+            RuntimeException {
         int[] value = new int[1];
         checkStatus(getParameter(DIRACSOUND_PARAM_MUSIC, value));
         return value[0];
     }
 
     public void setMusic(int enable) throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
+            IllegalArgumentException, UnsupportedOperationException,
+            RuntimeException {
         checkStatus(setParameter(DIRACSOUND_PARAM_MUSIC, enable));
     }
 
@@ -56,7 +58,8 @@ public class DiracSound extends AudioEffect {
     }
 
     public void setHeadsetType(int type) throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
+            IllegalArgumentException, UnsupportedOperationException,
+            RuntimeException {
         checkStatus(setParameter(DIRACSOUND_PARAM_HEADSET_TYPE, type));
     }
 
@@ -71,7 +74,8 @@ public class DiracSound extends AudioEffect {
     }
 
     public void setLevel(int band, float level) throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
+            IllegalArgumentException, UnsupportedOperationException,
+            RuntimeException {
         checkStatus(setParameter(new int[]{DIRACSOUND_PARAM_EQ_LEVEL, band},
                 String.valueOf(level).getBytes()));
     }
