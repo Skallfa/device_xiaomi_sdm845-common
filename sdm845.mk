16006aa40916bdf7c498b259db983b7fdbcbb476
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-06-27 19:04:57 +0530

    Update sdm845.mk

--- sdm845.mk
+++ sdm845.mk
@@ -406,7 +406,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service.xiaomi-sdm845-libperfmgr \
+    android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.xiaomi
 
 PRODUCT_COPY_FILES += \
