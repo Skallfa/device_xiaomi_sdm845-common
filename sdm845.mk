87423957ca636e25edd6242285763bcb145352e6
Skallfa <128469350+Skallfa@users.noreply.github.com>
2024-01-05 14:40:10 +0530

    Update sdm845.mk

--- sdm845.mk
+++ sdm845.mk
@@ -139,7 +139,8 @@ PRODUCT_PACKAGES += \
     libldacBT_abr \
     libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
 # Bluetooth Library Deps
 PRODUCT_PACKAGES += \
